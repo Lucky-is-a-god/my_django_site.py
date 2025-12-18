f75a816517594b1221c58a6230a126abe302b375
Lucky-is-a-god <ebisu_is_a_god@icloud.com>
2025-12-19 00:58:24 +0300

    Delete generation_parol.py

--- generation_parol.py
+++
@@ -1,9 +0,0 @@
-import random
-lower = "qwertyuioplkjhgfdsazxcvbnm"
-upper = "QWERTYUIOPLKJHGFDSAZXCVBNM"
-numbers= "0123456789"
-symbols = "{}[]()*;/,._-"
-all = lower + upper + numbers + symbols
-length = 16
-password = "". join(random.sample(all,length))
-print(password)
