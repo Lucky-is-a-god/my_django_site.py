556aa23e724be210a1ca28954412494675781b54
Lucky-is-a-god <ebisu_is_a_god@icloud.com>
2025-12-19 00:58:49 +0300

    Delete registers_in_a_string_and_its_examples.py

--- registers_in_a_string_and_its_examples.py
+++
@@ -1,8 +0,0 @@
-name = str(input('введите имя'))
-family = str(input('введите фамилию'))
-
-print( f'"Привет! {name} {family}')
-print( f' "Привет!" \n {name} {family}')
-full = f"{name} {family}"
-print(f"привет! {full.title()}")
-# так же бывают и другие виды title, upper (пишет весь текст капсом), lower(пишет все буквы маленькими)
